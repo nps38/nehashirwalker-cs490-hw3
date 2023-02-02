8b7d9d1750c755b48fcdc44750cf5fd9ecb2a7b4
Neha Shirwalker <nps38@njit.edu>
2023-02-02 01:15:13 -0500

    edit format of comment component

--- web/src/components/CommentsCell/CommentsCell.js
+++ web/src/components/CommentsCell/CommentsCell.js
@@ -1,7 +1,12 @@
+import Comment from 'src/components/Comment'
+
 export const QUERY = gql`
   query CommentsQuery {
     comments {
       id
+      name
+      body
+      createdAt
     }
   }
 `
@@ -11,15 +16,15 @@ export const Loading = () => <div>Loading...</div>
 export const Empty = () => <div>Empty</div>
 
 export const Failure = ({ error }) => (
-  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
 )
 
 export const Success = ({ comments }) => {
   return (
-    <ul>
-      {comments.map((item) => {
-        return <li key={item.id}>{JSON.stringify(item)}</li>
-      })}
-    </ul>
+    <div className="space-y-8">
+      {comments.map((comment) => (
+        <Comment key={comment.id} comment={comment} />
+      ))}
+    </div>
   )
 }
