16b57836e3a866c1118e83d281a23cbf01844fc4
Neha Shirwalker <nps38@njit.edu>
2023-02-05 03:18:13 -0500

    return all posts regardless of user

--- api/src/services/posts/posts.js
+++ api/src/services/posts/posts.js
@@ -1,32 +1,11 @@
 import { db } from 'src/lib/db'
 
 export const posts = () => {
-  return db.post.findMany({ where: { userId: context.currentUser.id } })
+  return db.post.findMany()
 }
 
 export const post = ({ id }) => {
-  return db.post.findFirst({
-    where: { id, userId: context.currentUser.id },
-  })
-}
-
-export const createPost = ({ input }) => {
-  return db.post.create({
-    data: { ...input, userId: context.currentUser.id },
-  })
-}
-
-export const updatePost = ({ id, input }) => {
-  return db.post.update({
-    data: input,
-    where: { id },
-  })
-}
-
-export const deletePost = ({ id }) => {
-  return db.post.delete({
-    where: { id },
-  })
+  return db.post.findUnique({ where: { id } })
 }
 
 export const Post = {
